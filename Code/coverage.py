3ef2b3052378525858fd5cdaa0d6b82ba8023a9e
DuaneHarris0813 <duaneharris@lanl.gov>
2024-05-06 11:44:05 -0600

    Modified HLA Functions

--- Code/coverage.py
+++ Code/coverage.py
@@ -7,11 +7,11 @@ Created on Tue Oct 10 16:18:13 2023
 """
 
 # THIS IS THE BRANCH FILE
+# Updated from Anaconda
 
 # IMPORT PACKAGES:
-from numpy import exp
 from numpy import unique
-import openpyxl as op
+from openpyxl import load_workbook
 import hla_functions as hf
 
 
@@ -25,41 +25,17 @@ fr2 = 'Individual Results'
 
 # DATA FILES:
 
-
-fa =  'HLA-A Allele Frequencies.xlsx'               # File for Allele Frequency
-fs = ['HLA-A_Ebola_Zaire_GP1_Weighted_Results.xlsx',
-      'HLA-A_Ebola_Sudan_GP1_Weighted_Results.xlsx',      
-      'HLA-A_Ebola_Zaire_NP_Weighted_Results.xlsx',
-      'HLA-A_Ebola_Sudan_NP_Weighted_Results.xlsx',
-      'HLA-A_SARS_Wuhan-Hu-1_Weighted_Results.xlsx',      
-      'HLA-A_SARS_DeltaAY4_Weighted_Results.xlsx',
-      'HLA-A_SARS_OmicronBA1_Weighted_Results.xlsx',
-      'HLA-A_SARS_OmicronBA2_Weighted_Results.xlsx',
-      'HLA-A_SARS_OmicronBA5_Weighted_Results.xlsx',
-      'HLA-A_Burkholderia_HCP1_Weighted_Results.xlsx']
-lb = ['Ebola GP1 (Zaire)',
-      'Ebola GP1 (Sudan)',
-      'Ebola NP (Zaire)',
-      'Ebola NP (Sudan)',
-      'SARS-CoV-2 Wuhan-Hu-1',
-      'SARS-CoV-2 Delta AY.4',
-      'SARS-CoV-2 Omicron BA.1',
-      'SARS-CoV-2 Omicron BA.2',
-      'SARS-CoV-2 Omicron BA.5', 
-      'Burkholderia HCP1']
-
-
-# fa =  'HLA-B Allele Frequencies.xlsx'               # File for Allele Frequency
-# fs = ['HLA-B_Ebola_Zaire_GP1_Weighted_Results.xlsx',
-#       'HLA-B_Ebola_Sudan_GP1_Weighted_Results.xlsx',      
-#       'HLA-B_Ebola_Zaire_NP_Weighted_Results.xlsx',
-#       'HLA-B_Ebola_Sudan_NP_Weighted_Results.xlsx',
-#       'HLA-B_SARS_Wuhan-Hu-1_Weighted_Results.xlsx',      
-#       'HLA-B_SARS_DeltaAY4_Weighted_Results.xlsx',
-#       'HLA-B_SARS_OmicronBA1_Weighted_Results.xlsx',
-#       'HLA-B_SARS_OmicronBA2_Weighted_Results.xlsx',
-#       'HLA-B_SARS_OmicronBA5_Weighted_Results.xlsx',
-#       'HLA-B_Burkholderia_HCP1_Weighted_Results.xlsx']
+# fa =  'HLA-A Allele Frequencies.xlsx'               # File for Allele Frequency
+# fs = ['HLA-A_Ebola_Zaire_GP1_Weighted_Results.xlsx',
+#       'HLA-A_Ebola_Sudan_GP1_Weighted_Results.xlsx',      
+#       'HLA-A_Ebola_Zaire_NP_Weighted_Results.xlsx',
+#       'HLA-A_Ebola_Sudan_NP_Weighted_Results.xlsx',
+#       'HLA-A_SARS_Wuhan-Hu-1_Weighted_Results.xlsx',      
+#       'HLA-A_SARS_DeltaAY4_Weighted_Results.xlsx',
+#       'HLA-A_SARS_OmicronBA1_Weighted_Results.xlsx',
+#       'HLA-A_SARS_OmicronBA2_Weighted_Results.xlsx',
+#       'HLA-A_SARS_OmicronBA5_Weighted_Results.xlsx',
+#       'HLA-A_Burkholderia_HCP1_Weighted_Results.xlsx']
 # lb = ['Ebola GP1 (Zaire)',
 #       'Ebola GP1 (Sudan)',
 #       'Ebola NP (Zaire)',
@@ -71,6 +47,27 @@ lb = ['Ebola GP1 (Zaire)',
 #       'SARS-CoV-2 Omicron BA.5', 
 #       'Burkholderia HCP1']
 
+fa =  'HLA-B Allele Frequencies.xlsx'               # File for Allele Frequency
+fs = ['HLA-B_Ebola_Zaire_GP1_Weighted_Results.xlsx',
+      'HLA-B_Ebola_Sudan_GP1_Weighted_Results.xlsx',      
+      'HLA-B_Ebola_Zaire_NP_Weighted_Results.xlsx',
+      'HLA-B_Ebola_Sudan_NP_Weighted_Results.xlsx',
+      'HLA-B_SARS_Wuhan-Hu-1_Weighted_Results.xlsx',      
+      'HLA-B_SARS_DeltaAY4_Weighted_Results.xlsx',
+      'HLA-B_SARS_OmicronBA1_Weighted_Results.xlsx',
+      'HLA-B_SARS_OmicronBA2_Weighted_Results.xlsx',
+      'HLA-B_SARS_OmicronBA5_Weighted_Results.xlsx',
+      'HLA-B_Burkholderia_HCP1_Weighted_Results.xlsx']
+lb = ['Ebola GP1 (Zaire)',
+      'Ebola GP1 (Sudan)',
+      'Ebola NP (Zaire)',
+      'Ebola NP (Sudan)',
+      'SARS-CoV-2 Wuhan-Hu-1',
+      'SARS-CoV-2 Delta AY.4',
+      'SARS-CoV-2 Omicron BA.1',
+      'SARS-CoV-2 Omicron BA.2',
+      'SARS-CoV-2 Omicron BA.5', 
+      'Burkholderia HCP1']
 
 # fa =  'HLA-C Allele Frequencies.xlsx'               # File for Allele Frequency
 # fs = ['HLA-C_Ebola_Zaire_GP1_Weighted_Results.xlsx',
@@ -99,104 +96,31 @@ lb = ['Ebola GP1 (Zaire)',
 ta  = 25 # Top Alleles (Max 25)
 
 
-# NORMALIZATION OPTIONS:
-en = 2 # Enrichment Normalization: 1=Rescale (Old Way) 2=Divide by Sum (New Way)
-ft = 2 # Frequency Type: 1=Sum, 2=Average
-
-
 #%% PARAMETERS ################################################################
-    
-
-# Log Enrichment Scores
-le = [0.127, #A
-     -0.175, #C
-      0.072, #D
-      0.325, #E
-      0.380, #F
-      0.110, #G
-      0.105, #H
-      0.432, #I
-     -0.700, #K
-     -0.036, #L
-     -0.570, #M
-     -0.021, #N
-     -0.036, #P
-     -0.376, #Q
-      0.168, #R
-     -0.537, #S
-      0.126, #T
-      0.134, #V
-      0.719, #W
-     -0.012] #Y
-    
-    
-# Position Importance
-pi = [0,    #1
-      0,    #2
-      0.1,  #3
-      0.31, #4
-      0.3,  #5
-      0.29, #6
-      0.26, #7
-      0.18, #8
-      0]    #9
 
 
-#%% NORMALIZATION #############################################################
-
-
-# NORMALIZE POSITION IMPORTANCE:
-S = sum(pi)
-for i in range(len(pi)):
-    pi[i] = pi[i]/S                 # Normalize so that sum(p[i])=1
-
-
-# NORMALIZE ENRICHMENT SCORES:
-for i in range(len(le)):
-    le[i] = exp(le[i])              # Undo Natural Log
-M = max(le); m = min(le)            # M = Max Enrichment, m = Min Enrichment
-S = sum(le)
-for i in range(len(le)):        
-    if en==1:       
-        le[i] = (le[i] - m) / (M-m) # Rescale Enrichment Scores
-    if en==2:
-        le[i] = le[i]/S             # Normalize Enrichment Scores
-        
-        
-# CREATE ENRICHMENT SCORE DICTIONARY:
-aa = list('ACDEFGHIKLMNPQRSTVWY')
-es = dict(zip(aa,le))
+# GET POSITION WEIGHTS AND ENRICHMENT SCORES:
+pw = hf.compute_position_weights()  # Position Weights
+es = hf.compute_enrichment_scores() # Enrichment Scores
 
 
 #%% LOAD ALLELE FREQUENCY DATA ################################################
 
 
-a  = op.load_workbook(fd+fa)
+a  = load_workbook(fd+fa)
 rs = a.sheetnames
 nr = len(rs)
 af = [{} for i in range(nr)]
 Z  = []
 for i in range(nr):
-    ad = []
-    if ft==1: m1=18; m2=19
-    if ft==2: m1=12; m2=13
-    for k in a[rs[i]].iter_rows(min_row=3, min_col=m1, max_col=m2, values_only=True):
-        if isinstance(k[0], str): ad.append(k)
-        else: break
-    na = len(ad)
-    S = 0
-    for k in range(na):
-        af[i]['HLA-'+ad[k][0]]=ad[k][1]
-        S = S + ad[k][1]
+    af[i],S = hf.allele_frequency_dict(a[rs[i]])
     Z.append(S)
-    for k in af[i]:
-        af[i][k] = af[i][k]/S
         
         
 #%% LOAD PROTEIN SEQUENCE DATA ################################################
 
 
-dp = op.load_workbook(fd+fp)
+dp = load_workbook(fd+fp)
 ps = []
 for i,j in enumerate(dp['Phase 1'].iter_rows(min_row=2, max_row=11, min_col=3, max_col=3, values_only=True)):
     ps.append(j[0])
@@ -256,8 +180,8 @@ for i in range(nd): # Iterate Through Diseases
 
 
     # LOAD DATA FOR DISEASE i:
-    if isinstance(fs,str): d = op.load_workbook(fd+fs)   
-    else: d = op.load_workbook(fd+fs[i])
+    if isinstance(fs,str): d = load_workbook(fd+fs)   
+    else: d = load_workbook(fd+fs[i])
     
         
     for j in range(nr): # Iterate Through Regions
@@ -317,11 +241,11 @@ for i in range(nd): # Iterate Through Diseases
                 sg[i][j].append(0)
             else:
                 ia = [l for l in range(MN) if a1[l] == a2[k]]
-                sg[i][j].append(hf.compute_sigma([b1[l] for l in ia],[p1[l] for l in ia],es,pi))
+                sg[i][j].append(hf.compute_sigma([b1[l] for l in ia],[p1[l] for l in ia],es,pw))
                 
                 
         # COMPUTE REGIONAL COVERAGE SCORE:
-        c1[i].append(hf.compute_C(a1,b1,p1,af[j],es,pi))
+        c1[i].append(hf.compute_C(a1,b1,p1,af[j],es,pw))
         
         
         # COMPUTE INDIVIDUAL COVERAGE SCORES:
@@ -332,7 +256,7 @@ for i in range(nd): # Iterate Through Diseases
 
 
 # LOAD RESULTS SHEETS:
-r = op.load_workbook(fdr+fr1)
+r = load_workbook(fdr+fr1)
 
 
 # ALLELE TYPE:
@@ -377,7 +301,7 @@ r.save(fdr+fr1)
 for j in range(nr):   
     
     # LOAD RESULT FILE:
-    r = op.load_workbook(fdr+fr2+' '+rs[j]+'.xlsx')
+    r = load_workbook(fdr+fr2+' '+rs[j]+'.xlsx')
      
     # FREQUENCY:
     w = r['Frequency']
--- Code/hla_functions.py
+++ Code/hla_functions.py
@@ -8,7 +8,9 @@ Created on Wed Feb 28 10:11:33 2024
 
 
 # IMPORT PACKAGES:
+from numpy import divide
 from numpy import dot
+from numpy import exp
 from numpy import matmul
 from numpy import multiply
 from numpy import unique
@@ -16,11 +18,72 @@ from numpy import vectorize
 from statistics import mean
 
 
+# DICTIONARY FUNCTION:
 def dict_value(d,k):
     return d[k]
 
 
-# IMMUNOGENICITY
+# POSITION WEIGHTS:
+def compute_position_weights():
+    # ORIGINAL POSITION WEIGHTS
+    pw = [0,    #1
+          0,    #2
+          0.1,  #3
+          0.31, #4
+          0.30, #5
+          0.29, #6
+          0.26, #7
+          0.18, #8
+          0]    #9
+    return(divide(pw,sum(pw))) # Normalize Position Weights
+
+
+# ENRICHMENT SCORES:
+def compute_enrichment_scores():
+    # ORIGINAL LOG ENRICHMENT SCORES:
+    es = [0.127, #A
+         -0.175, #C
+          0.072, #D
+          0.325, #E
+          0.380, #F
+          0.110, #G
+          0.105, #H
+          0.432, #I
+         -0.700, #K
+         -0.036, #L
+         -0.570, #M
+         -0.021, #N
+         -0.036, #P
+         -0.376, #Q
+          0.168, #R
+         -0.537, #S
+          0.126, #T
+          0.134, #V
+          0.719, #W
+         -0.012] #Y
+    
+    # NORMALIZE ENRICHMENT SCORES:
+    es = exp(es)
+    es = divide(es,sum(es))
+    
+    # CREATE ENRICHMENT SCORE DICTIONARY:
+    aa = list('ACDEFGHIKLMNPQRSTVWY')
+    return(dict(zip(aa,es)))
+
+
+# ALLELE FREQUENCY DICTIONARY:
+def allele_frequency_dict(w):
+    af = {}
+    for k in w.iter_rows(min_row=3,min_col=12,max_col=13,values_only=True):
+        if isinstance(k[0],str): af['HLA-'+k[0]] = k[1]
+        else: break
+    S = sum(af.values())    
+    for k in af:
+        af[k] = af[k]/S         
+    return(af,S)
+    
+    
+# IMMUNOGENICITY:
 def compute_g(p,es,pw):
     g0 = [list(i) for i in p]
     g1 = vectorize(dict_value)(es,g0)
@@ -97,35 +160,3 @@ def compute_F(a,p,b,af,es,pw,idp):
             sn = sn + t
     F = sn/sd
     return F
-
-
-# # INDIVIDUAL COVERAGE RATIO:
-# def compute_H(a,p,b,af,es,pw,idp):
-    
-#     # UNIQUE ALLELES:
-#     an = unique(a) # Allele Names
-#     M  = len(an)   # Number of Alleles
-#     MN = len(a)    # Number of Alleles * Number of Epitopes
-    
-#     # COMPUTE INDIVIDUAL FREQUENCIES:
-#     f = []                  # Initialize Allele Frequency
-#     for i in range(M):      # Iterate Though Alleles
-#         f.append(af[an[i]]) # Get Frequency of Allele i
-#     rho = compute_rho(f)    # Compute Individual Frequencies
-    
-#     # COMPUTE SIGMA FOR EACH ALLELE:
-#     sg = []
-#     for i in range(M):
-#         ai = [j for j in range(MN) if a[j] == an[i]]
-#         sg.append(compute_sigma([b[j] for j in ai],[p[j] for j in ai],es,pw))
-#     I = compute_I(sg)
-        
-#     # COMPUTE SIGMA DOMINANT FOR EACH ALLELE:
-#     sd = []
-#     for i in range(M):
-#         ai = [j for j in range(MN) if a[j] == an[i] & p[j] in idp]
-#         sd.append(compute_sigma([b[j] for j in ai],[p[j] for j in ai],es,pw))
-#     I = compute_I(sd)
-    
-    
-        
